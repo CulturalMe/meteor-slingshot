6e9d1febb1094890337ba6ba752400e0ae3eae48
gsuess <garic.suess@gmail.com>
2014-11-26 17:52:43 +0100

    Package definition

--- package.js
+++ package.js
@@ -1,12 +1,15 @@
 Package.describe({
   name: "edgee:slingshot",
   summary: "Directly post files to cloud storage services, such as AWS-S3.",
-  version: "0.2.0"
+  version: "0.0.1",
+  git: "https://github.com/CulturalMe/meteor-slingshot"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@1.0.0');
+
   api.use(["underscore", "check"]);
-  api.use("tracker", "client");
+  api.use(["tracker", "reactive-var"], "client");
 
   api.add_files("lib/upload.js", "client");
   api.add_files([
